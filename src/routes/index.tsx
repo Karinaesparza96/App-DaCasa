e9bac1c0c67f346aea4705538f4d6b4d71462253
Karina Esparza <karinaesparza296@gmail.com>
2023-04-11 16:01:49 -0300

    alterando espaçamento imagem Inicio.modules.scss

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import Header from 'components/Header';
 import NavBar from 'components/NavBar';
 import Inicio from 'pages/Inicio';
 import Menu from 'pages/Menu';
+import Sobre from 'pages/Sobre';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const AppRouter = () => {
@@ -13,6 +14,7 @@ const AppRouter = () => {
                     <Route path='/' element={<Header />}>
                         <Route index element={<Inicio />} />
                         <Route path='menu' element={<Menu />} />
+                        <Route path='sobre' element={<Sobre/>}/>
                     </Route>
                 </Routes>
             </Router>
