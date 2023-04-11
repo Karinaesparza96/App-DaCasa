d3294435820bb88b04370c50745925757ba34086
Karina Esparza <karinaesparza296@gmail.com>
2023-04-11 18:41:51 -0300

    criando pagina detalhes do prato

---
+++ src/pages/Detalhes/index.tsx
@@ -0,0 +1,13 @@
+import styles from './Detalhes.module.scss';
+
+const Detalhes = () => {
+    return (
+        <section>
+            <div>
+                <h1>Teste</h1>
+            </div>
+        </section>
+    );
+};
+
+export default Detalhes;
